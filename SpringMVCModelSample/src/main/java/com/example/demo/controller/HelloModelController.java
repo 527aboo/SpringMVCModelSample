a33b43ebe380efc1f289156a2ff4c9f148a62d97
大場修 <o.oba725@gmail.com>
2023-08-21 09:51:28 +0900

    初回コミット

---
+++ SpringMVCModelSample/src/main/java/com/example/demo/controller/HelloModelController.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.model.Member;
+
+@Controller
+@RequestMapping("hello")
+public class HelloModelController {
+	
+	@GetMapping("model")
+	public String helloView(Model model) {
+		
+		Member mb = new Member();
+		mb.setId(1);
+		mb.setName("次郎");
+
+		Member mb2 = new Member();
+		mb2.setId(2);
+		mb2.setName("三郎");
+
+		List<String> list = new ArrayList<>();
+		list.add("東");
+		list.add("西");
+		list.add("南");
+		list.add("北");
+		
+		Map<String, Member> map = new HashMap<>();
+		map.put("tanaka", mb);
+		map.put("suzuki", mb2);		
+		
+		model.addAttribute("msg", "タイムリーフ");
+		model.addAttribute("name", "太郎");
+		model.addAttribute("number", 123);
+		model.addAttribute("mb", mb);
+		model.addAttribute("list", list);
+		model.addAttribute("map", map);
+		return "helloThymeleaf";
+	}
+}
